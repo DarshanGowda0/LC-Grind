fda29d9890eca9453f4b40081bb728644ba57c06
DarshanGowda0 <darshan.gowda008@gmail.com>
2020-09-11 21:08:45 -0700

    Day 68

--- Daily-Grind/68.py
+++ Daily-Grind/68.py
@@ -1,9 +1,12 @@
 class Solution:
     def reconstructQueue(self, people: List[List[int]]) -> List[List[int]]:
+        # [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+        # sort by height
+        # [7,0][7,1][6,1][5,0][5,2][4,4]
+        # [5,0][7,0][5,2][6,1][4,4][7,1]
         
-        people.sort(key = lambda x : (-x[0], x[1]))
+        people.sort(key=lambda x: (-x[0],x[1]))
         res = []
         for h, k in people:
-            res.insert(k, [h,k])
-            
+            res.insert(k,[h,k])
         return res
