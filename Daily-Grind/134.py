ec6dee6aa4c4f5cae4adfb6fefc1660c6fbc3893
DarshanGowda0 <darshan.gowda008@gmail.com>
2020-09-19 19:40:15 -0700

    Day 76

--- Daily-Grind/134.py
+++ Daily-Grind/134.py
@@ -26,3 +26,28 @@ class Solution:
             return dp[idx]
         
         return recur(0, {})
+
+# second attempt
+
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        
+        def dfs(s, idx, dp):
+            if idx == len(s):
+                return 1
+            
+            if s[idx] == '0':
+                return 0
+            
+            if idx == len(s) - 1:
+                return 1
+            
+            if idx not in dp:
+                count1 = dfs(s, idx+1, dp)
+                count2 = dfs(s, idx+2, dp) if int(s[idx: idx+2]) <= 26 else 0
+
+                dp[idx] = count1+count2
+            
+            return dp[idx]
+        
+        return dfs(s, 0, {})
