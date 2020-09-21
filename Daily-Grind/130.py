af6b7033ce0ac3ca916c2da9bb709339e267cc7f
DarshanGowda0 <darshan.gowda008@gmail.com>
2020-09-20 19:57:46 -0700

    Day 77

--- Daily-Grind/130.py
+++ Daily-Grind/130.py
@@ -28,5 +28,35 @@ class Solution:
             return node
         
         return recur(0, len(preorder) - 1)
+
+# second attempt
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        
+        inOrder = {val: idx for idx, val in enumerate(inorder)}
+        que = deque(preorder)
+        
+        def recur(start, end):
+            if start > end:
+                return None
+            
+            val = que.popleft()
+            idx = inOrder[val]
+            node = TreeNode(val)
+            node.left = recur(start, idx-1)
+            node.right = recur(idx+1, end)
+            
+            return node
+        
+        return recur(0, len(preorder) - 1)
             
             
