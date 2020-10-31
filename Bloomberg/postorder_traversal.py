647596791eaab5794bf2e4f0d2c29e2e08953eaa
DarshanGowda0 <darshan.gowda008@gmail.com>
2020-10-30 22:19:58 -0700

    Day 117

--- Bloomberg/postorder_traversal.py
+++ Bloomberg/postorder_traversal.py
@@ -6,9 +6,34 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: TreeNode) -> List[int]:
+        """
         def recur(node):
             if not node:
                 return []
             return recur(node.left) + recur(node.right) + [node.val]
         
         return recur(root)
+        """
+        
+        # iterative
+        stack = []
+        res = []
+        
+        while root or stack:
+            while root:
+                if root.right:
+                    stack += root.right,
+                stack += root,
+                root = root.left
+                
+            root = stack.pop()
+            
+            if stack and stack[-1] == root.right:
+                stack[-1] = root
+                root = root.right
+            else:
+                res += root.val,
+                root = None
+                
+        
+        return res
