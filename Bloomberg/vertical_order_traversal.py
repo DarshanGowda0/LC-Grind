617a881618fcf7afc24d7e25c747da0c1f394661
DarshanGowda0 <darshan.gowda008@gmail.com>
2020-11-13 21:37:06 -0800

    Day 131

--- Bloomberg/vertical_order_traversal.py
+++ Bloomberg/vertical_order_traversal.py
@@ -31,4 +31,36 @@ class Solution:
         return [table[x] for x in range(minCol, maxCol + 1)]
                 
             
+# second attempt
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def verticalTraversal(self, root: TreeNode) -> List[List[int]]:
+        node = (0, 0, root)
+        que = deque([node])
+        res = []
+        while que:
+            # print(que)
+            x, y, node = que.popleft()
+            res += (x, y, node.val),
+            if node.left:
+                que += (x-1, y-1, node.left),
+                
+            if node.right:
+                que += (x+1, y-1, node.right),
+                
+        res.sort(key = lambda x: (x[0], -x[1], x[2]))
+        
+        minX, maxX = res[0][0], res[-1][0]
+        ans = [[] for _ in range(maxX-minX+1)]
+        for x, y, val in res:
+            ans[x - minX] += val,
             
+        return ans
