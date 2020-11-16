e25a2066b49e9e8e59eed0febeb562754cf1c2b9
DarshanGowda0 <darshan.gowda008@gmail.com>
2020-11-15 19:14:37 -0800

    Day 133

---
+++ Bloomberg/maximal_square.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix:
+            return 0
+        m, n = len(matrix), len(matrix[0])
+        dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        res = 0
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == '1':
+                    dp[i+1][j+1] = min(dp[i][j], dp[i+1][j], dp[i][j+1]) + 1
+                    res = max(res, dp[i+1][j+1])
+                    
+        return res*res
+        
+        
